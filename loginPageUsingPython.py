1c0076c1e3ee0687c418c315e5c5b22bc72a41d5
Patric Phinehas Raj <patricphinehas@gmail.com>
2019-02-12 01:00:19 +0530

    Add files via upload

--- loginPageUsingPython.py
+++ loginPageUsingPython.py
@@ -1,7 +1,13 @@
 def check(user):
     import csv
     # print("1")
-    with open('details.txt', mode='r') as data:
+    f = 'details.txt'
+    # f = str(findFile(userName))
+    # print(f)
+    # h = "'"
+    # j = h + f + h
+    # print(j)
+    with open(f, mode='r') as data:
         # print("2")
         csv_read = csv.DictReader(data)
         linec = 1
@@ -18,6 +24,20 @@ def check(user):
                 linec+=1
                 # print("4")
     return res
+
+def findFile(userName):
+    year = userName[0:2]
+    dept = userName[2:4].upper()
+    roll = userName[4:6]
+    print("year: ",year," dept: ",dept," roll: ",roll)
+    new = "".join((year,dept))
+    t = '.txt'
+    print(new)
+    file = new + t
+    print(file)
+    return file
+
+
 while(1):       
     userName = str(input("enter the username"))
     test = check(userName)
